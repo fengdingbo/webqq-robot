24db480911f868a656548a48f15b52d964e5d220
fengdingbo <fengdingbo@gmail.com>
2013-06-04 22:41:14 +0800

    添加注释说明

--- constants.php
+++ constants.php
@@ -1,9 +1,17 @@
 <?php
-// 2838975848 123456789
-// 1953024456 robot123
-// 2838572550 robot123
-define("USERNAME","");
-define("PASSWORD","");
-define("REPLY","parse.ini");
-define("temp_dir","tmp/");
+//======================*/
+/*  测试帐号			*/
+/* 2838975848 123456789	*/
+/* 1953024456 robot123	*/
+/* 2838572550 robot123	*/
+
+define("USERNAME","");	// qq帐号
+define("PASSWORD","");	// qq密码
+define("REPLY","parse.ini"); // 相关回复解析
+define("temp_dir","tmp/"); // cookie，日志等信息的临时存放目录
+
+//==================*/
+/* 是否开启日志		*/
+/*     0:关闭		*/
+/*     1:开启		*/
 define("log_threshold",1);
